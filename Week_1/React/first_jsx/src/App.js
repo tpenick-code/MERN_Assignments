069878dd8a1f84d2d394de4dbe785bfd113fa7e2
tpenick-code <77137134+tpenick-code@users.noreply.github.com>
2021-08-14 16:23:27 -0400

    Adding Assignment

---
+++ Week_1/React/first_jsx/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import list from './components/list';
+
+function App() {
+  return (
+    <div className="App">
+     <list />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week_1/React/first_jsx/src/components/list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const list = () => {
+    return (
+        <div>
+            <h1>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+      </div>
+    )
+}
+
+export default list;
