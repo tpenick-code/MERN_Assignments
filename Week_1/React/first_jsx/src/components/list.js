e14145eeeac06b893302f5050b3b3f41814e2baa
tpenick-code <77137134+tpenick-code@users.noreply.github.com>
2021-08-14 16:31:23 -0400

    Updating Assignment

--- Week_1/React/first_jsx/src/components/list.js
+++ Week_1/React/first_jsx/src/components/list.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const list = () => {
     return (
-        <div>
+        <div className="list">
             <h1>Hello Dojo!</h1>
             <h2>Things I need to do:</h2>
-                <li>Learn React</li>
+            <ul>
+            <li>Learn React</li>
                 <li>Climb Mt. Everest</li>
                 <li>Run a marathon</li>
                 <li>Feed the dogs</li>
+            </ul>
       </div>
-    )
+    );
 }
 
 export default list;
