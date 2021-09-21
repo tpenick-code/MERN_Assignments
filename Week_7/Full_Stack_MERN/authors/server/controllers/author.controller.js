ddf6cb38a8082a5dfaa7decebf36e7b48050bc60
tpenick-code <77137134+tpenick-code@users.noreply.github.com>
2021-09-21 18:34:59 -0400

    Updating Week_7 Assignment

---
+++ Week_7/Full_Stack_MERN/authors/server/controllers/author.controller.js
@@ -0,0 +1,43 @@
+const Author = require("../models/author.model");
+
+module.exports = {
+
+    getAllAuthors: (req, res) => {
+        Author.find({})
+            .then((allAuthors)=>res.json(allAuthors))
+            .catch((err)=>res.status(400).json(err))
+    },
+
+    getOneAuthor: (req, res) => {
+        Author.findById({_id: req.params._id})
+            .then((oneAuthor)=>res.json(oneAuthor))
+            .catch((err)=>res.status(400).json(err))
+    },
+
+    createAuthor: (req, res) => {
+        Author.create(req.body)
+            .then((newAuthor)=>res.json({author: newAuthor}))
+            .catch((err)=>res.status(400).json(err))
+    },
+
+
+    updateAuthor: (req, res) => {
+        Author.findByIdAndUpdate(
+            {_id: req.params._id},
+            req.body,
+            {
+            new: true,
+            runValidators: true
+            })
+            .then((updatedAuthor)=>res.json(updatedAuthor))
+            .catch((err)=>res.status(400).json(err))
+    },
+
+    deleteAuthor: (req, res) => {
+        Author.deleteOne({_id: req.params._id})
+            .then((deleteId)=>res.json(deleteId))
+            .catch((err)=>res.status(400).json(err))
+    }
+
+
+}
