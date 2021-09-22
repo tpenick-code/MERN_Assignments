d59b23304bd2671c70caa9036249fdc193652b4c
tpenick-code <77137134+tpenick-code@users.noreply.github.com>
2021-09-21 22:04:37 -0400

    Updating Week_7 Assignment

--- Week_7/Full_Stack_MERN/authors/server/models/author.model.js
+++ Week_7/Full_Stack_MERN/authors/server/models/author.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const AuthorSchema = new mongoose.Schema({
-    name:{
+    authorName:{
         type: String,
         required: [true, "Name is required"],
         minlength: [3, "Name must be at least 3 characters long"]
