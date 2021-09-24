bcf27f20dcac95b35e37590aef51178c474e7469
tpenick-code <77137134+tpenick-code@users.noreply.github.com>
2021-09-24 17:57:31 -0400

    Updating Week_8 Assignment

--- Week_8/OOP_and_Class_Components/putting_it_together/src/components/PersonCard.js
+++ Week_8/OOP_and_Class_Components/putting_it_together/src/components/PersonCard.js
@@ -1,14 +1,29 @@
-import React, {useState} from 'react';
+import React, { Component } from "react";
 
-const PersonCard = (props) => {
-    const [newAge, setNewAge] = useState(props.age);
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ageUp: this.props.age,
+    };
+  }
+  render() {
+ 
+    const { firstName, lastName, hair } = this.props;
+
     return (
-        <div>
-            <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {newAge}</p>
-            <p>Hair Color: {props.hairColor}</p>
-            <button onClick={(event) => setNewAge(newAge +1)}>Birthday Button for {props.firstName} {props.lastName}</button>
-        </div>
-    )
+      <div className="container">
+        <h2>
+          {lastName}, {firstName}
+        </h2>
+        <p>Age: {this.state.ageUp}</p>
+        <p>Hair Color: {hair}</p>
+        <button onClick={() => this.setState({ ageUp: this.state.ageUp + 1 })}>
+          Birthday!!
+        </button>
+      </div>
+    );
+  }
 }
 export default PersonCard;
